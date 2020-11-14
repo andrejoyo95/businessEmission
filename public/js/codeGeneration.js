fab1ca5a2a7015612fa4ca130bd5bf1c13bf95b0
AndreJoyo <afjoga@outlook.es>
2020-11-15 00:01:04 +0100

    Generar código al presionar enter

--- public/js/codeGeneration.js
+++ public/js/codeGeneration.js
@@ -14,43 +14,50 @@ function getCookie(cname) {
     return "";
 }
 let toGenerateCode = document.getElementById('toGenerateCode');
-toGenerateCode.addEventListener('click', function () {
-    let email = document.getElementById('email');
-    //let url = 'http://localhost:9000/saveVisualizationCode'
-    let url = 'https://visualization-codes-services.herokuapp.com/saveVisualizationCode'
-    axios.get(url, {
+toGenerateCode.addEventListener('click', generateCode);
+function comproveEnterPress(e) {
+  if (e.keyCode === 13) { //console.log(e.which);
+    console.log("enter presionado");
+    generateCode();
+  }
+}
+function generateCode() {
+  let email = document.getElementById('email');
+  //let url = 'http://localhost:9000/saveVisualizationCode'
+  let url = 'https://visualization-codes-services.herokuapp.com/saveVisualizationCode'
+  axios.get(url, {
     params: {
         email: email.value,
         event: eventStream,
         businessKey: key.innerHTML,
         idEmission: getCookie('idEmission')
     }
-    })
-    .then(res => {//console.log(res.data);
-        if (!res.data) {
-            console.log(' - ')
-        } else{
-            console.log(res.data);
-            let codeTable = document.getElementById('codeTable');
-            let tr = document.createElement('tr');
-            let tdTime = document.createElement('td');
-            let tdEmail = document.createElement('td');
-            let tdCode = document.createElement('td');
+  })
+  .then(res => {//console.log(res.data);
+      if (!res.data) {
+          console.log(' - ')
+      } else{
+          console.log(res.data);
+          let codeTable = document.getElementById('codeTable');
+          let tr = document.createElement('tr');
+          let tdTime = document.createElement('td');
+          let tdEmail = document.createElement('td');
+          let tdCode = document.createElement('td');
 
-            let date = new Date(res.data.time);
-            let seconds = date.getSeconds();
-            let minutes = date.getMinutes();
-            let hour = date.getHours();
-            let time = document.createTextNode(hour+':'+minutes+':'+seconds);
+          let date = new Date(res.data.time);
+          let seconds = date.getSeconds();
+          let minutes = date.getMinutes();
+          let hour = date.getHours();
+          let time = document.createTextNode(hour+':'+minutes+':'+seconds);
 
-            tdTime.append(time);
-            tr.append(tdTime);
-            tdEmail.innerHTML = email.value;
-            tr.append(tdEmail);
-            tdCode.innerHTML = res.data.code;
-            tr.append(tdCode);
-            codeTable.append(tr);
-            email.value=''
-        }
-    })    
-})
+          tdTime.append(time);
+          tr.append(tdTime);
+          tdEmail.innerHTML = email.value;
+          tr.append(tdEmail);
+          tdCode.innerHTML = res.data.code;
+          tr.append(tdCode);
+          codeTable.append(tr);
+          email.value=''
+      }
+  })
+}
