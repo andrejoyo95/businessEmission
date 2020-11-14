6131ad8ad6a6123a46eab4d4be654d1a1fa57905
AndreJoyo <afjoga@outlook.es>
2020-11-14 22:47:38 +0100

    Cambios menores

--- socket/socket.js
+++ socket/socket.js
@@ -1,14 +1,11 @@
-//let {getHosts} = require('../controllers/getHosts')
 //let {comproveEmissionKey} = require('../controllers/comproveEmissionKey')
 let {comproveVisualizationCode} = require('../controllers/comproveVisualizationCode')
-//let {addUsedCode} = require('../controllers/addUsedCode')
 
 // middleware
 io.use((socket, next) => {
 	console.log('middleware middleware middleware middleware middleware')
 	console.log(socket.handshake.headers)
 	let host = socket.handshake.headers.host
-	//let hosts = hosts.includes(host) getHosts()
 	if(host==='localhost:5000'){ 
 		console.log('host admitido')
 		console.log(host)
@@ -16,7 +13,6 @@ io.use((socket, next) => {
 	} else {
 		next(new Error('Host authentication error'))
 	}
-	/*if(validHost){console.log('host admitido')next()} else {next(new Error('Host authentication error'))}*///next()
 	console.log(host)
 	next()
 })
@@ -25,88 +21,18 @@ io.on('connection', async function(socket) {
 	let event = socket.handshake.query.eventStream
  	let key = socket.handshake.query.key
 	console.log('emissionKey: ', emissionKey, 'event: ', event, 'key: ', key)
-	console.log('socketsocketsocketsocketsocketsocketsocket')
 	console.log('COOKIE: ', socket.handshake.headers.cookie)
-//si idEmission(emissionKey) es válido, escuchar evento correspondiente en servidor(socket.on(event)) y emitir(socket.broadcast.emit(event))
-	//let validEmissionKey = await comproveEmissionKey(key, emissionKey)
-	
-	// COMPROBAR VALIDEZ DE EMISSIONKEY
-	
+		
 	if (emissionKey) { //code==='emission'  validEmissionKey
 		var fs = require('fs');
 		let num = {}
 		socket.on(event, function (image) {
 			if (event!='') {
-				if (num.event) {
-					num.event++
-				} else {
-					num.event = 1
-				}
-				function decodeBase64Image(dataString) {
-					var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
-					  response = {};
-				  
-					if (matches.length !== 3) {
-					  return new Error('Invalid input string');
-					}
-				  
-					response.type = matches[1];
-					response.data = new Buffer(matches[2], 'base64');
-				  
-					return response;
-				}
-				let img = decodeBase64Image(image)
-				console.log('------------')
-				console.log(num)
-				console.log(typeof(num.event))
-				let name = event+num.event+".jpg"
-				console.log(name)
-				fs.writeFile('images/'+name, img.data, function(err) { //... 
-					console.log(num)
-					console.log('err: ', err)
-					console.log(typeof(img))
-					console.log(typeof(image))
-					console.log(img)
-				})
 				
-				if (num.event===125){
-					let images = []
-					for (let i = 1; i < num.event; i++) {
- 
-						images.push('images/'+event+i+".jpg")
-						
-					}
-					console.log('........................')
-					console.log('........................')
-					console.log('........................')
-					console.log('........................')
-					console.log('........................')
-					console.log(images)
-					var videoshow = require('videoshow')
-					var videoOptions = {
-						//fps: 25,
-						loop: 0.125, // seconds 
-						transition: false, //transitionDuration: 0.1, // seconds
-						videoBitrate: 1024,
-						videoCodec: 'libx264',
-						size: '640x640', //audioBitrate: '128k', audioChannels: 2,
-						format: 'mp4',
-						pixelFormat: 'yuv420p'
-					}
-						
-					videoshow(images, videoOptions)
-					.save('videos/'+event+'.mp4')
-					.on('start', function (command) {
-						console.log('ffmpeg process started:', command)
-					})
-					.on('error', function (err, stdout, stderr) {
-						console.error('Error:', err)
-						console.error('ffmpeg stderr:', stderr)
-					})
-					.on('end', function (output) {
-						console.error('Video created in:', output)
-					})
-				}
+
+
+
+
 				socket.broadcast.emit(event, image)
 				console.log('eventCatched using code: ',event)
 			} else{
@@ -120,7 +46,6 @@ io.on('connection', async function(socket) {
 	} else {
 		let visualizationCode = socket.handshake.query.visualizationCode
 		let validVisualizationCode = await comproveVisualizationCode(key, visualizationCode)
-		console.log('qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq')
 		console.log(validVisualizationCode)
 		if(validVisualizationCode) {
 			let usedCodes =  validVisualizationCode.usedCodes
@@ -132,7 +57,6 @@ io.on('connection', async function(socket) {
 			} else {
 				console.log('-----Client socket connected-----')
 			}
-			//await addUsedCode(key, visualizationCode) //is called on visualizationcodesServices
 			console.log('Código usado ', currentNumberUses+1, ' veces')
 		} else {
 			socket.disconnect()
