f99a38058a420370b40dc21cc9234190539621bf
ajoyo <afjoga@outlook.es>
2021-03-01 19:02:12 +0100

    eliminando warning en consola

--- socket/socketBusiness.js
+++ socket/socketBusiness.js
@@ -2,8 +2,8 @@ let messageOnConsole = false
 async function socketBusiness(socket, eventStream) { //let fs = require('fs')let num = {}
 	console.log('-----Business socket connected-----')
 	socket.on(eventStream, function (image) {
-		if (!messageOnConsole) emissionWarning()
-		socket.broadcast.emit(eventStream, image)		
+		//if (!messageOnConsole) emissionWarning()
+		socket.broadcast.emit(eventStream, image)
 	})
 	socket.on('disconnect', () => {
 		console.log('----- Socket negocio desconectado-----')			
@@ -12,7 +12,7 @@ async function socketBusiness(socket, eventStream) { //let fs = require('fs')let
 
 module.exports.socketBusiness = socketBusiness
 
-function emissionWarning() {
+/*function emissionWarning() {
 	process.stdout.write("Emitiendo")
 	messageOnConsole = true
 	setTimeout(function () {
@@ -22,4 +22,4 @@ function emissionWarning() {
 		}, 1000)
 	}, 2000)
 	process.stdout.cursorTo(0)
-}
+}*/
