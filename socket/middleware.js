c27afba761d9a3d5ffa3e8489f736ca2289e1999
ajoyo <afjoga@outlook.es>
2021-04-07 22:16:10 +0200

    solucionando duplicidad de conexiones

--- socket/middleware.js
+++ socket/middleware.js
@@ -2,17 +2,11 @@ let { io } = require('./socket')
 // socket middleware
 io.use((socket, next) => {
 	console.log()
-	console.log()
 	console.log('--------------------socket middleware--------------------')
 	//console.log(socket.handshake.headers)
 	let host = socket.handshake.headers.host
-	if(host==='localhost:5000'){ 
-		console.log('host admitido')
-		next()
-	} else {
-		next(new Error('Host authentication error'))
-	}
-	//console.log(host)
+	console.log('host:', host)
+	console.log(socket.handshake.query)
 	next()
 })
 
